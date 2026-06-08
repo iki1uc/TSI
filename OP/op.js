8c235e4d22f0f09b1166589d0f81b2fa15ebfb36
iki1uc <entertrainer@web.de>
2026-06-08 02:14:12 +0200

    Update op.js

--- OP/op.js
+++ OP/op.js
@@ -1,1 +1,18 @@
+import { WG } from './wg.js';
+import { loadYML } from './loadYML.js';
+
+export async function OP(value) {
+  const yml = await loadYML();
+
+  const wg = parseFloat(yml.wg || WG);
+
+  return {
+    operator: yml.operator,
+    axis: yml.axis,
+    input: value,
+    wg,
+    output: value * wg,
+    state: value * wg > 0.5 ? "aktiv" : "passiv"
+  };
+}
 
