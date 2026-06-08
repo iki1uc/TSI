f3671c9ebbd57037be18df47612cabe84232a584
iki1uc <entertrainer@web.de>
2026-06-08 02:06:06 +0200

    Update op.js

--- UPD/op.js
+++ UPD/op.js
@@ -1,1 +1,10 @@
+// op.js – Operator für dieses Werkzeug
 
+export function OP(value, wg) {
+  return {
+    input: value,
+    wg,
+    output: value * wg,
+    state: value * wg > 0.5 ? "aktiv" : "passiv"
+  };
+}
