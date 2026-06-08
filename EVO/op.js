1b76506454f4abdb1ae2b9904f656fa96f170db7
iki1uc <entertrainer@web.de>
2026-06-08 02:06:23 +0200

    Update op.js

--- EVO/op.js
+++ EVO/op.js
@@ -1,1 +1,11 @@
+// op.js – Operator für dieses Werkzeug
+
+export function OP(value, wg) {
+  return {
+    input: value,
+    wg,
+    output: value * wg,
+    state: value * wg > 0.5 ? "aktiv" : "passiv"
+  };
+}
 
