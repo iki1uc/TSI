7cd635d17dd371c3a1160361111d2d33d7ba1805
iki1uc <entertrainer@web.de>
2026-06-08 18:37:57 +0200

    Update aufstellung.js

--- br/aufstellung.js
+++ br/aufstellung.js
@@ -1,1 +1,20 @@
+// erzeugt 81 oder 81(1)
+
+export function makeAufstellung(molekuel, kontroll) {
+
+    if (!kontroll) {
+        return {
+            mode: "81",
+            share: 0.81,
+            molekuel
+        };
+    }
+
+    return {
+        mode: "81(1)",
+        share: 0.82,
+        molekuel,
+        kontroll
+    };
+}
 
