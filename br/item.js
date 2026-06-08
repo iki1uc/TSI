6da7ec1d98512ca96195d8365e3fb47f165832c7
iki1uc <entertrainer@web.de>
2026-06-08 18:37:33 +0200

    Update item.js

--- br/item.js
+++ br/item.js
@@ -1,1 +1,12 @@
+// erzeugt ein Item aus einem Segment der RAUM.yml
+
+export function makeItem(segmentName, segmentData) {
+    return {
+        name: segmentName,
+        share: segmentData.share,
+        fragment: segmentData.fragment,
+        operator: segmentData.operator,
+        bind: segmentData.bind || null
+    };
+}
 
