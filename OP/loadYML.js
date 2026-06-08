42dbadd6465166ed227a4eb82ac2cadd253cbd02
iki1uc <entertrainer@web.de>
2026-06-08 02:13:03 +0200

    Update loadYML.js

--- OP/loadYML.js
+++ OP/loadYML.js
@@ -1,1 +1,17 @@
+export async function loadYML() {
+  const res = await fetch('./op.yml');
+  const text = await res.text();
+
+  const lines = text.split('\n');
+  const data = {};
+
+  for (const line of lines) {
+    if (line.includes(':')) {
+      const [key, value] = line.split(':').map(s => s.trim());
+      data[key] = value;
+    }
+  }
+
+  return data;
+}
 
